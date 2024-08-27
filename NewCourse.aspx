ff0413730b075865b8814c260c2c03970cab8cce
Lavanya <lavssubbu@gmail.com>
2024-08-27 19:18:09 +0530

    NewCourse

---
+++ NewCourse.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NewCourse.aspx.cs" Inherits="WebApplication6.Course" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Course Management</title>
+</head>
+<body>
+   <form id="form1" runat="server">
+        <div>
+            <h2>Add Course</h2>
+            <asp:Label ID="lblCourseName" runat="server" Text="Course Name:"></asp:Label>
+            <asp:TextBox ID="txtCourseName" runat="server"></asp:TextBox><br />
+
+            <asp:Label ID="lblCourseDesc" runat="server" Text="Course Description:"></asp:Label>
+            <asp:TextBox ID="txtCourseDesc" runat="server" TextMode="MultiLine"></asp:TextBox><br />
+
+            <asp:Label ID="lblCoursePrice" runat="server" Text="Course Price:"></asp:Label>
+            <asp:TextBox ID="txtCoursePrice" runat="server"></asp:TextBox><br />
+
+            <asp:Label ID="lblCourseDuration" runat="server" Text="Course Duration:"></asp:Label>
+            <asp:TextBox ID="txtCourseDuration" runat="server"></asp:TextBox><br />
+
+            <asp:Label ID="lblCourseCat" runat="server" Text="Course Category:"></asp:Label>
+            <asp:TextBox ID="txtCourseCat" runat="server"></asp:TextBox><br />
+
+            <asp:Label ID="lblImageUpload" runat="server" Text="Course Image:"></asp:Label>
+            <asp:FileUpload ID="FileUpload1" runat="server" /><br />
+
+            <asp:Button ID="btnSignup" runat="server" Text="Add Course" OnClick="btnSignup_Click" /><br />
+
+            <h2>Update/Delete Course</h2>
+            <asp:Label ID="lblCourseId" runat="server" Text="Course ID:"></asp:Label>
+            <asp:TextBox ID="IDboxUD" runat="server"></asp:TextBox><br />
+
+            <asp:Button ID="FetchData" runat="server" Text="Fetch Data" OnClick="FetchData_Click" /><br />
+
+            <asp:Button ID="Updatelbl" runat="server" Text="Update Course" OnClick="Updatelbl_Click" />
+            <asp:Button ID="Deletelbl" runat="server" Text="Delete Course" OnClick="Deletelbl_Click" /><br />
+
+            <asp:Label ID="Label1" runat="server" Text="Update Status" Visible="False"></asp:Label>
+            <asp:Label ID="Label2" runat="server" Text="Delete Status" Visible="False"></asp:Label>
+           
+            <h2>Course List</h2>
+             <asp:Image ID="imgCourse" runat="server"
+                    ImageUrl='<%# Eval("courseimage", "data:image/jpeg;base64,{0}") %>' 
+                    Width="100px" Height="100px" />
+           <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CssClass="table table-bordered">
+    <Columns>
+        <asp:BoundField DataField="courseid" HeaderText="Course ID" />
+        <asp:BoundField DataField="coursetitle" HeaderText="Title" />
+        <asp:BoundField DataField="coursedescription" HeaderText="Description" />
+        <asp:BoundField DataField="courseprice" HeaderText="Price" />
+        <asp:BoundField DataField="courseduration" HeaderText="Duration" />
+        <asp:BoundField DataField="coursecategory" HeaderText="Category" />
+        <asp:TemplateField HeaderText="Image">
+            <ItemTemplate>
+                <asp:Image ID="imgCourse" runat="server" ImageUrl='<%# GetImageUrl(Eval("courseimage")) %>' Width="100px" Height="100px" />
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ NewCourse.aspx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WebApplication6
+{
+    public partial class Course : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("data source=LAPTOP-BQF0DTHQ\\SQLEXPRESS;database=samp2024;integrated security=true;");
+                con.Open();
+            DisplayData();
+
+            con.Close();
+        }
+        protected void DisplayData()
+        {
+            SqlCommand command = new SqlCommand("SELECT * FROM courselist", con);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+        protected void btnSignup_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+           byte[] imageData = null;
+            if (FileUpload1.HasFile)
+            {
+                string fileName =
+               Path.GetFileName(FileUpload1.PostedFile.FileName);
+                string fileExtension = Path.GetExtension(fileName);
+                filePath = "Images/" + fileName;
+                FileUpload1.SaveAs(Server.MapPath(filePath));
+                // Read file data as binary
+                using (BinaryReader reader = new BinaryReader(FileUpload1.PostedFile.InputStream))
+                {
+                    imageData = reader.ReadBytes(FileUpload1.PostedFile.ContentLength);
+                }
+            }
+           
+            string username = txtCourseName.Text;
+            string description = txtCourseDesc.Text;
+            int price = int.Parse(txtCoursePrice.Text);
+            int duration = int.Parse(txtCourseDuration.Text);
+            string category = txtCourseCat.Text;
+
+            cmd = new SqlCommand("INSERT INTO courselist(coursetitle,courseimage,coursedescription,courseduration,courseprice,coursecategory) VALUES(@username,@imageData,@description,@duration,@price,@category)", con);
+            cmd.Parameters.AddWithValue("@username", username);
+           cmd.Parameters.AddWithValue("@imageData", (object)imageData ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@description", description);
+            cmd.Parameters.AddWithValue("@duration", duration);
+            cmd.Parameters.AddWithValue("@price", price);
+            cmd.Parameters.AddWithValue("@category", category);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Course Added");
+            DisplayData();
+            con.Close();
+        }
+
+        protected void FetchData_Click(object sender, EventArgs e)
+        {
+            int getdata = int.Parse(IDboxUD.Text);
+            FetchRecordById(getdata);
+        }
+        private void FetchRecordById(int id)
+        {
+            con.Open();
+            cmd = new SqlCommand("SELECT * FROM courselist WHERE courseid=@id",
+           con);
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    txtCourseName.Text = reader.GetString(reader.GetOrdinal("coursetitle"));
+                    txtCourseDesc.Text = reader.GetString(reader.GetOrdinal("coursedescription"));
+                    txtCoursePrice.Text = reader.GetDecimal(reader.GetOrdinal("courseprice")).ToString();
+                    txtCourseDuration.Text = reader.GetInt32(reader.GetOrdinal("courseduration")).ToString();
+                    txtCourseCat.Text = reader.GetString(reader.GetOrdinal("coursecategory"));
+
+                    // Retrieve binary data
+                    byte[] imageData = reader["courseimage"] as byte[];
+                    if (imageData != null)
+                    {
+                        // Convert to Base64 string for display
+                        string base64String = Convert.ToBase64String(imageData);
+                        imgCourse.ImageUrl = "data:image/png;base64," + base64String; // Adjust MIME type if necessary
+                    }
+                }
+            }
+            else
+            {
+                // Show error message
+            }
+            reader.Close();
+            con.Close();
+        }
+
+
+        protected void Updatelbl_Click(object sender, EventArgs e)
+        {
+            byte[] imageData = null;
+            if (FileUpload1.HasFile)
+            {
+                string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                FileUpload1.SaveAs(Server.MapPath("Images/" + fileName));
+                // Read file data as binary
+                using (BinaryReader reader = new BinaryReader(FileUpload1.PostedFile.InputStream))
+                {
+                    imageData = reader.ReadBytes(FileUpload1.PostedFile.ContentLength);
+                }
+            }
+
+            using (SqlConnection con = new SqlConnection("data source=LAPTOP-BQF0DTHQ\\SQLEXPRESS;database=samp2024;integrated security=true;"))
+            {
+                con.Open();
+                cmd = new SqlCommand("UPDATE courselist SET coursetitle=@title, coursedescription=@description, courseprice=@price, courseduration=@duration, coursecategory=@category, courseimage=@imageData WHERE courseid=@id", con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(IDboxUD.Text));
+                cmd.Parameters.AddWithValue("@title", txtCourseName.Text);
+                cmd.Parameters.AddWithValue("@description", txtCourseDesc.Text);
+                cmd.Parameters.AddWithValue("@price", decimal.Parse(txtCoursePrice.Text));
+                cmd.Parameters.AddWithValue("@duration", int.Parse(txtCourseDuration.Text));
+                cmd.Parameters.AddWithValue("@category", txtCourseCat.Text);
+                cmd.Parameters.Add("@imageData", SqlDbType.VarBinary).Value = (object)imageData ?? DBNull.Value;
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Course Updated");
+                DisplayData();
+            }
+        }
+
+        protected void Deletelbl_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            int id = int.Parse(IDboxUD.Text);
+            // Delete related records in usertbl table
+
+            cmd = new SqlCommand("UPDATE Users SET courseid = NULL WHERE courseid = @id", con);
+           
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+            // Delete record in courselist table
+            cmd = new SqlCommand("DELETE FROM courselist WHERE courseid=@id",
+           con);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Course Deleted");
+            DisplayData();
+            con.Close();
+
+        }
+        // Add this method to your class
+        public string GetImageUrl(object imageData)
+        {
+            if (imageData != DBNull.Value)
+            {
+                byte[] bytes = (byte[])imageData;
+                string base64String = Convert.ToBase64String(bytes);
+                return "data:image/jpeg;base64," + base64String;
+            }
+            return string.Empty;
+        }
+    }
+}
