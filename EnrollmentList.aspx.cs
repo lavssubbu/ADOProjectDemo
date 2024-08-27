c0f722464be6ed565bdb1c4ad7f2ba6d6d5679ac
Lavanya <lavssubbu@gmail.com>
2024-08-27 19:22:19 +0530

    EnrollmentList

---
+++ EnrollmentList.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class EnrollmentList1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ADOConn"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                SELECT u.Userid, u.Username, u.Password, u.courseid, c.coursetitle, u.CourseProgress
+                FROM Users u
+                INNER JOIN courselist c ON u.courseid = c.courseid";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
